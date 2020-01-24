f311ba8b3c5bb3438ee7c12f6eb3459edb00f5ab
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-24 22:16:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,4 +2,5 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Ch
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "male","brown", "playful", 1 );
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "female","brown", "shy", 1 );
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 6, "male","brown", "inquisitive", 1 );
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "female","cinnamon", "shy", 1 );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "female","cinnamon", "shy", 1 );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 6, "male","brown", "shy", 1 );
